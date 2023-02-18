4d615555439b569c3dac15403a3049ece905fdff
john <johncardona1992@gmail.com>
2023-02-18 16:19:38 -0500

    End

---
+++ src/main/scala/PassByValue.scala
@@ -0,0 +1,13 @@
+object PassByValue extends App {
+  def callByValue(x: Long): Unit = {
+    println("by value:" + x)
+    println("by value:" + x)
+  }
+  def callByName(x: => Long): Unit = {
+    println("by name:" + x)
+    println("by name:" + x)
+  }
+
+  callByValue(System.nanoTime())
+  callByName(System.nanoTime())
+}
